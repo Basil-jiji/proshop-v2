d0c4f12a6a742fd78eb345068bcbd7088390ccb8
Basil Jiji <basiljiji2015@gmail.com>
2023-07-27 14:03:13 +0530

    Redux Store and API Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
-} from 'react-router-dom'
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,13 +23,15 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
-)
+);
 
-reportWebVitals()
+reportWebVitals();
