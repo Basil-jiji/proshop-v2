9679987d2a444eee89976b1f64ab9f41c956ab19
Basil Jiji <basiljiji2015@gmail.com>
2023-08-10 10:55:01 +0530

    User Email & Password Validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,24 @@ import User from '../models/userModel.js    ';
 // @route  POST /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('auth user');
+  // console.log(req.body);
+
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //If there is a user or else part
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc   Register user
