4c25370d7672b2a4366670741d9d51ad0869faaa
Basil Jiji <basiljiji2015@gmail.com>
2023-08-11 11:30:02 +0530

    User Registration and Hashing Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from '../middleware/asyncHandler.js';
-import User from '../models/userModel.js    ';
-import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc   Auth user & get token
 // @route  POST /api/users/login
@@ -12,18 +12,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   //If there is a user or else part
   if (user && (await user.matchPassword(password))) {
-    //Generate JWT Token
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-
-    //Set JWT as HTTP-Only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000, //30 Days
-    });
+    generateToken(res, user._id);
 
     res.json({
       _id: user._id,
@@ -41,7 +30,34 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route  POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc   Logout user & clear cookie
@@ -53,7 +69,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     expires: new Date(0),
   });
 
-  res.status(200).json({ message: 'Logged Out Successfully'})
+  res.status(200).json({ message: 'Logged Out Successfully' });
 });
 
 // @desc   Get user profile
