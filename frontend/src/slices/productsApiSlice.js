edefaaceaca7355ae9eefd45fda8159728e32b79
Basil <basiljiji2015@gmail.com>
2024-01-09 23:48:15 +0530

    Create Reviews - Frontend

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -44,9 +44,24 @@ export const productApiSlice = apiSlice.injectEndpoints({
         url: `${PRODUCTS_URL}/${productId}`,
         method: 'DELETE'
       })
+    }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data
+      }),
+      invalidatesTags: ['Product']
     })
   }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation } =
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery, 
+  useCreateProductMutation, 
+  useUpdateProductMutation, 
+  useUploadProductImageMutation, 
+  useDeleteProductMutation, 
+  useCreateReviewMutation } =
   productApiSlice;
