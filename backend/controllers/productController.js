6963d6d98c153d7a58d8091fd0d2095c48bff5cf
Basil <basiljiji2015@gmail.com>
2024-01-11 00:54:34 +0530

    Product Carousel

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,11 +11,11 @@ const getProducts = asyncHandler(async (req, res) => {
 
   const keyword = req.query.keyword ? { name: { $regex: req.query.keyword, $options: "i" } } : {};
 
-  const count = await Product.countDocuments({...keyword});
+  const count = await Product.countDocuments({ ...keyword });
 
 
 
-  const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page - 1));
+  const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1));
   res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
@@ -133,4 +133,19 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview };
+// @desc   Get top rated products
+// @route  GET /api/product/top
+// @access Public
+// const getTopProducts = asyncHandler(async (req, res) => {
+//   const products = await Product.findById({}).sort({ rating: -1 }).limit(3);
+
+//   res.status(200).json(products);
+// });
+
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
+export { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview, getTopProducts };
